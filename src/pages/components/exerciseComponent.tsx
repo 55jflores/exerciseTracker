6fcf97e1e139e29521183a2ea0e104c7cbd3410c
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-30 13:30:08 -0700

    Added box styling to exercises

--- src/pages/components/exerciseComponent.tsx
+++ src/pages/components/exerciseComponent.tsx
@@ -10,20 +10,22 @@ interface ExercisesProps {
 const ExerciseComponent = ({exercises, workout}: ExercisesProps) => {
 
 return (
-    <div>
-
+<>
     {exercises && exercises.map((exercise) => (
-        <Link key={exercise.id} href='/exercise/[workout]/[exerciseID]/[exercise]/[exerciseDate]/[exerciseSet1]/[exerciseSet2]/[exerciseSet3]' as={`/exercise/${workout}/${exercise.id}/${exercise.exercise}/${exercise.date}/${exercise.set1}/${exercise.set2}/${exercise.set3}`}>
-            <div key={exercise.id} className="flex flex-col items-center my-4">
-                <h2 className="text-center text-green-500 text-xl font-bold">{exercise.exercise}: {exercise.date}</h2>
-                <p className="mt-2">Set1: {exercise.set1}</p>
-                <p className="mt-2">Set2: {exercise.set2}</p>
-                <p className="mt-2">Set3: {exercise.set3}</p>
-            </div>
-        </Link>
-        ))}
+        <div className="bg-gray-300 dark:bg-gray-700 p-4 rounded-lg shadow-lg mb-4">
+            <Link key={exercise.id} href='/exercise/[workout]/[exerciseID]/[exercise]/[exerciseDate]/[exerciseSet1]/[exerciseSet2]/[exerciseSet3]' as={`/exercise/${workout}/${exercise.id}/${exercise.exercise}/${exercise.date}/${exercise.set1}/${exercise.set2}/${exercise.set3}`}>
+                <div key={exercise.id} className="flex flex-col items-center my-4">
+                    <h2 className="text-center text-green-500 text-xl font-bold">{exercise.exercise}: {exercise.date}</h2>
+                    <p className="text-gray-800 dark:text-white mt-2">Set1: {exercise.set1}</p>
+                    <p className="text-gray-800 dark:text-white mt-2">Set2: {exercise.set2}</p>
+                    <p className="text-gray-800 dark:text-white mt-2">Set3: {exercise.set3}</p>
+                </div>
+            </Link>
+        </div>
 
-    </div>
+    ))}
+
+</>
 
 )
 }
