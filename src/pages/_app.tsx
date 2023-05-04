3fdf64c0b8a7a888dc87479194e15b5691f2df03
Jesus Flores <jesus.flores1008@gmail.com>
2023-05-04 14:31:58 -0700

    Updated api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import NavBar from './components/navBar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <NavBar/>
       <Component {...pageProps} />
 
     </div>
--- src/pages/api/selectClient.ts
+++ src/pages/api/selectClient.ts
@@ -5,13 +5,20 @@ import supabaseClient from './supabaseclient';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+const tadah = process.env.NEXT_PUBLIC_TADAH
 //export const supabaseClient = createClient<Database>(supabaseUrl as string, supabaseAnonKey as string)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
   const body = req.body  
   const split = body.workout as string
-  const { data, error } = await supabaseClient.from(split).select('*')
+  const myIn = body.tadah as string
+
+  if (tadah == myIn){
+    const { data, error } = await supabaseClient.from(split).select('*')  
+    res.json(data ?? [])
+  }
 
   // Return JSON
-  res.json(data ?? [])
+  res.json([])
 }
