da38c384f371a2216191b94d2641d3f11c3e0fa9
Jesus Flores <jesus.flores1008@gmail.com>
2023-05-04 14:34:52 -0700

    Fixed up api

--- src/pages/api/selectClient.ts
+++ src/pages/api/selectClient.ts
@@ -5,7 +5,7 @@ import supabaseClient from './supabaseclient';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const tadah = process.env.NEXT_PUBLIC_TADAH
+const tadah = process.env.NEXT_PUBLIC_TADAH as string;
 //export const supabaseClient = createClient<Database>(supabaseUrl as string, supabaseAnonKey as string)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
